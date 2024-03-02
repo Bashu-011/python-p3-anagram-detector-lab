70a99e19a2469d2264b9435efe35eb86e5020a48
Bashu-011 <bashu2609@gmail.com>
2024-03-02 08:10:28 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class  Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        anagram = []
+        for w in words:
+            if sorted(self.word) == sorted(w.lower()):
+                anagram.append(w)
+        return anagram
+
+
